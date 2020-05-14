eabd4df9b2f83bae0a3a456f9436047585f1cddb
Ed <lookatemail@gmail.com>
2020-05-14 08:48:38 +0100

    able to pull data from DB

--- lib/thermostat.rb
+++ lib/thermostat.rb
@@ -8,4 +8,9 @@ class Thermostat
     db_connect.exec("INSERT INTO data_saved (temperature, PSM, city) VALUES ('#{temperature}', '#{psm}', '#{city}');")
   end
 
+  def self.list_from_DB
+    db_connect = PG.connect dbname: 'thermostat'
+    db_connect.exec("SELECT * FROM data_saved;")
+  end
+
 end
--- spec/thermostat_spec.rb
+++ spec/thermostat_spec.rb
@@ -10,4 +10,13 @@ describe Thermostat do
     end
   end
 
+  it 'able to list data from DB' do
+    Thermostat.save_to_DB(temperature: '23', psm: 'false', city: 'Tokio')
+    thermostat = Thermostat.list_from_DB
+
+    thermostat.each do |db|
+      expect(db['psm']).to include 'false'
+    end
+  end
+
 end
